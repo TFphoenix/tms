8150a3e5eebd104b716982e07563a796f5006f37
Teodor <teodor@mihaescu.info>
2022-09-27 20:21:04 +0200

    Dynamic title functionality

--- mulee-interface/src/app/app.component.ts
+++ mulee-interface/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'MENU';
+  title: string = 'MENU';
+  readonly titles: { [id: string]: string } = {
+    '/breakfast/predefined': 'CHOOSE YOUR BREAKFAST',
+    '/breakfast/customize': 'CUSTOMIZE YOUR BREAKFAST',
+    '/breakfast/explore': 'EXPLORE YOUR BREAKFAST',
+  };
+
+  constructor(private readonly _router: Router) {
+    _router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.title = this.titles[val.urlAfterRedirects];
+      }
+    });
+  }
 }
--- mulee-interface/src/app/pages/predefined-breakfast/predefined-breakfast.component.ts
+++ mulee-interface/src/app/pages/predefined-breakfast/predefined-breakfast.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Breakfast } from 'src/app/models/breakfast.model';
 import { ModalService } from 'src/app/shared/modal/modal.service';
-import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-predefined-breakfast',
@@ -9,14 +8,11 @@ import { Output, EventEmitter } from '@angular/core';
   styleUrls: ['./predefined-breakfast.component.scss'],
 })
 export class PredefinedBreakfastComponent implements OnInit {
-  @Output() navbarTitle = new EventEmitter<string>();
   breakfastOptions: Breakfast[] = Array<Breakfast>(8);
 
   constructor(private readonly _modal: ModalService) {}
 
-  ngOnInit(): void {
-    this.navbarTitle.emit('CHOOSE YOUR BREAKFAST');
-  }
+  ngOnInit(): void {}
 
   openModal(id: string) {
     this._modal.open(id);
