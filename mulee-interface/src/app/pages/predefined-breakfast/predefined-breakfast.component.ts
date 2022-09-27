2b71abf8467a82f3c4d4a0b1dae74a6f8175dde1
Teodor <teodor@mihaescu.info>
2022-09-27 17:19:18 +0200

    arduino comm tests

--- mulee-interface/src/app/pages/predefined-breakfast/predefined-breakfast.component.ts
+++ mulee-interface/src/app/pages/predefined-breakfast/predefined-breakfast.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Breakfast } from 'src/app/models/breakfast.model';
 import { ModalService } from 'src/app/shared/modal/modal.service';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-predefined-breakfast',
@@ -8,11 +9,14 @@ import { ModalService } from 'src/app/shared/modal/modal.service';
   styleUrls: ['./predefined-breakfast.component.scss'],
 })
 export class PredefinedBreakfastComponent implements OnInit {
+  @Output() navbarTitle = new EventEmitter<string>();
   breakfastOptions: Breakfast[] = Array<Breakfast>(8);
 
   constructor(private readonly _modal: ModalService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.navbarTitle.emit('CHOOSE YOUR BREAKFAST');
+  }
 
   openModal(id: string) {
     this._modal.open(id);
--- mulee-tests/classic.js
+++ mulee-tests/classic.js
@@ -13,6 +13,22 @@ port.pipe(parser);
 // Read the data from the serial port
 parser.on("data", (line) => console.log(line));
 
-// Write the data to the serial port
-port.write("ROBOT POWER ON");
-port.write("ROBOT POWER ON");
+// Open errors will be emitted as an error event
+port.on('error', function (err) {
+    console.log('Error: ', err.message)
+})
+
+// const encoder = new TextEncoder();
+// const writer = port.writable.getWriter();
+// await writer.write(encoder.encode("PING"));
+// writer.releaseLock();
+
+setInterval(function () {
+    // Write the data to the serial port
+    port.write("ROBOT POWER ON\r\n", function (err) {
+        console.log(err);
+    });
+    // port.flush();
+
+    console.log("Message sent");
+}, 100);
