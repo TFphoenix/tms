2ca929878639bed7d806fcad4564010e59934eef
Paul Pasch <panter2804@gmail.com>
2022-09-29 20:59:09 +0200

    Fixed Pin Definitions

---
+++ mulee-arduino/src/main/pins.ino
@@ -0,0 +1,55 @@
+const int X_Step = A0;
+const int X_Dir = A1;
+const int X_En = A2;
+
+const int Y_Step = A6;
+const int Y_Dir = A7;
+const int Y_En = A8;
+
+const int Z_Step = 46;
+const int Z_Dir = 48;
+const int Z_En = 62;
+
+const int E0_Step = 26;
+const int E0_Dir = 28;
+const int E0_En = 24;
+
+const int E1_Step = 36;
+const int E1_Dir = 34;
+const int E1_En = 30;
+
+const int xmax = 2;
+const int xmin = 3;
+
+const int G_STEP_PIN = 46;
+const int G_DIR_PIN = 48;
+const int G_ENABLE_PIN = 62;  // Active LOW
+
+struct stepper {
+  int enable_pin;
+  int direction_pin;
+  int step_pin;
+};
+
+//PIN Definitions
+
+
+
+
+//PIN Definitions
+struct stepper stepperX = { X_En, X_Dir, X_Step };
+struct stepper stepperY = { Y_En, Y_Dir, Y_Step };
+struct stepper stepperZ = { Z_En, Z_Dir, Z_Step };
+struct stepper stepperE0 = { E0_En, E0_Dir, E0_Step };
+struct stepper stepperE1 = { E1_En, E1_Dir, E1_Step };
+
+
+struct stepper dispensers[4] = { stepperX, stepperY, stepperE0, stepperE1 };
+
+
+struct stepper g_stepper3 = { 62, 48, 46 };
+
+
+
+//struct stepper stepper1 = {38, 55, 54};  //X
+//struct stepper stepper2 = {56, 61, 60};  //
